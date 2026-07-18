e79276b810382d28a15918881c2e7dfd933e42c5
Bivaas <bivaasbaral7@gmail.com>
2026-07-18 22:20:05 +0545

    navbar buttons

--- Frontend/src/components/navbar.component.jsx
+++ Frontend/src/components/navbar.component.jsx
@@ -10,6 +10,26 @@ const Navbar = () => {
                 <img src={logo} className="w-full" />
 
             </Link>
+
+            <div className="absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto">
+
+                <input 
+                type="text"
+                placeholder="Search"
+                className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark md:pl-12"
+                />
+
+                <i className="fi fi-rr-search absolute right-10 md:pointer-events-none md:left-5 md:top-1/2 md:-translate-y-1/2 text-xl text-dark-grey"></i>
+
+            </div>
+
+            <div className="flex items-center gapp-3 md:gap-6 ml-auto">
+
+                <button className="md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center"></button>
+
+                    <i className="fi fi-rr-search text-xl"></i>
+            </div>
+
         </nav>
     )
 }
