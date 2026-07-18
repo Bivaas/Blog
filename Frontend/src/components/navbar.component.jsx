6f24643f796e384b3fd15e6fa684c90a0fc4fcb4
Bivaas <bivaasbaral7@gmail.com>
2026-07-18 22:30:33 +0545

    search icon and button container added

--- Frontend/src/components/navbar.component.jsx
+++ Frontend/src/components/navbar.component.jsx
@@ -1,17 +1,21 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../imgs/logo.png";
 
 const Navbar = () => { 
 
+    const [ searchBoxVisibility, setSearchVisibility ] = useState(false);
+
     return ( 
         <nav className="navbar">
+
             <Link to="/" className="flex-none w-10">
             
                 <img src={logo} className="w-full" />
 
             </Link>
 
-            <div className="absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto">
+            <div className={"absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show " + (searchBoxVisibility ? "show" : "hide")}>
 
                 <input 
                 type="text"
@@ -25,9 +29,13 @@ const Navbar = () => {
 
             <div className="flex items-center gapp-3 md:gap-6 ml-auto">
 
-                <button className="md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center"></button>
+                <button className="md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center" 
+                
+                onClick={() => setSearchBoxVisibility(currentVal => !currentVal)}>
 
                     <i className="fi fi-rr-search text-xl"></i>
+
+                </button>
             </div>
 
         </nav>
