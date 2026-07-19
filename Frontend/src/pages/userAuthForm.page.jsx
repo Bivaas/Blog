e350fc510f277e5637e4e380d0afebb0eb583966
Bivaas <bivaasbaral7@gmail.com>
2026-07-19 14:18:40 +0545

    Auth form setup done

---
+++ Frontend/src/pages/userAuthForm.page.jsx
@@ -0,0 +1,22 @@
+const UserAuthForm = ({ type }) => {
+
+    return ( 
+        
+        <section className="h-cover flex itemms-center justify-center">
+
+            <form className="w-[80%] max-w-[400px]">
+
+                <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
+                    {type == "sign-in" ? "Welcome back" : "Join us today" }
+                </h1>
+                
+            </form>
+
+        </section>
+
+    )
+}
+
+
+
+export default UserAuthForm
