58e2036073d5e4ca750d8a5f56020076a5f7d900
Bivaas <bivaasbaral7@gmail.com>
2026-07-19 20:09:55 +0545

    mongoDB atlas connection and env setup

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose'
 
 
 const server = express();
 let PORT = 3000;
+
+
+mongoose.connect(process.env.DB_LOCATION, {
+    autoIndex: true
+})
+
 
 server.listen(PORT, () => { 
     console.log('listening on port ' + PORT);
