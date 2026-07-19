4827a25301d5aefdc2c67e4fb0d4e1a5d8a588d2
Bivaas <bivaasbaral7@gmail.com>
2026-07-19 15:29:34 +0545

    Input form update and styled

--- Frontend/src/components/input.component.jsx
+++ Frontend/src/components/input.component.jsx
@@ -1,8 +1,36 @@
-const InputBox = () => { 
+import { useState } from "react";
+
+const InputBox = ({  name, type, id, value, placeholder, icon }) => {
+    
+    const [ passwordVisible, setPasswordVisible ] = useState(false);
 
     return ( 
+        <div className="relative w-[100%] mb-4">
 
-        <h1>Input box</h1>
+            <input 
+            name={name}
+            type={type == "password" ? passwordVisible ? "text" : "password" : type }
+            placeholder={placeholder}
+            defaultValue={value}
+            id={id}
+            className="input-box"
+            />
+
+            <i className={"fi " + icon + " input-action"}></i>
+
+            { 
+                type == "password" ?
+
+                <i className={"fi fi-rr-eye" + (!passwordVisible ? "-crossed" : "") + " input-icon left-[auto] right-4 cursor-pointer"}
+                onclick={ () => setPasswordVisible(currentVal => !currentVal)}>
+
+                </i>
+                : ""
+            }
+
+
+        </div>
+       
     )
 }
 
