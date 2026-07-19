8485eada901a1d8735cf66bf548d3e329c1411a7
Bivaas <bivaasbaral7@gmail.com>
2026-07-19 14:44:23 +0545

    Input form for new user

---
+++ Frontend/src/components/input.component.jsx
@@ -0,0 +1,10 @@
+const InputBox = () => { 
+
+    return ( 
+
+        <h1>Input box</h1>
+    )
+}
+
+
+export default InputBox
--- Frontend/src/pages/userAuthForm.page.jsx
+++ Frontend/src/pages/userAuthForm.page.jsx
@@ -1,3 +1,5 @@
+import InputBox from "../components/input.component";
+
 const UserAuthForm = ({ type }) => {
 
     return ( 
@@ -9,7 +11,34 @@ const UserAuthForm = ({ type }) => {
                 <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
                     {type == "sign-in" ? "Welcome back" : "Join us today" }
                 </h1>
-                
+
+
+                  { 
+                   type != "sign-up" ? 
+                        <InputBox 
+                        name="fullname"
+                        type="text"
+                        placeholder="Your Full Name.."
+                        icon="fr-rr-usr"
+                        />
+                        : ""
+                  }
+
+                    <InputBox
+                        name="email"
+                        type="email"
+                        placeholder="Your email.."
+                        icon="fi-rr-envelope"
+                    />
+
+                    <InputBox
+                    name="password"
+                    type="password"
+                    placeholder="Your top secret password"
+                    icon="fi-rr-key"
+                    />
+
+
             </form>
 
         </section>
