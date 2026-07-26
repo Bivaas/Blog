532630da01ce28eaac2cd2995de14f4404cbbd6a
Bivaas <bivaasbaral7@gmail.com>
2026-07-26 07:49:54 +0545

    banner setup and basic editor page layout

--- Frontend/src/components/blog-editor.component.jsx
+++ Frontend/src/components/blog-editor.component.jsx
@@ -1,8 +1,56 @@
+import { Link } from "react-router-dom";
+import logo from "../imgs/logo.png";
+import defaultBanner from "../imgs/blog banner.png"
+
+
 const BlogEditor = () => { 
 
 return (
 
-    <h1>blog editor from component jsx file</h1>
+    <>
+    
+    <nav>
+
+        <Link to="/">
+            <img src={logo} />
+        </Link>
+
+        <p>New Blog</p>
+
+        <div>
+
+            <button>Publish</button>
+            <button>Save Draft</button>
+        </div>
+
+    </nav>
+
+
+    <section>
+
+        <div>
+            <div>
+
+                <label htmlFor="uploadBanner">
+
+                    <img src={defaultBanner} />
+
+                    <input 
+
+                    id="uploadBanner"
+                    type="file"
+                    accept=".png, .jpg, .jpeg"
+                    hidden />
+
+                    
+                </label>
+
+            </div>
+        </div>
+
+    </section>
+
+    </>
 )
 }
 
