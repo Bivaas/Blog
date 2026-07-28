0d8e4694b66d78244fea905bedf1f088e22e6983
Bivaas <bivaasbaral7@gmail.com>
2026-07-28 07:30:56 +0545

    editorjs package added and tools mapped to its plugin

---
+++ Frontend/src/components/tools.component.jsx
@@ -0,0 +1,37 @@
+import Embed from "@editorjs/embed";
+import List from "@editorjs/List";
+import Image from "@editorjs/image";
+import Header from "@editorjs/header";
+import Quote from "@editorjs/quote";
+import Marker from "@editorjs/marker";
+import InlineCode from "@editorjs/inline-code";
+
+
+
+export const tools = {
+
+    embed: Embed, 
+    list: {
+
+        class: List,
+        inlineToolbar: true
+    },
+
+    header: {
+        class: Header,
+        config: {
+
+            placeholder: "your heading...",
+            levels: [2, 3],
+            defaultLevel: 2
+        }
+    },
+
+    quote: {
+
+        class: Quote,
+        inlineToolbar: true
+    },
+    marker: Marker,
+    inlineCode: InlineCode
+}
