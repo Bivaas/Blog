5b8ffd61643a8823b03b5681c5a7d46a6319fb00
Bivaas <bivaasbaral7@gmail.com>
2026-08-24 21:09:16 +0545

    route set to default and home page added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,6 +5,7 @@ import UserAuthForm from "./pages/userAuthForm.page";
 import { createContext, useEffect, useState } from "react";
 import { lookInSession } from "./common/session";
 import Editor from "./pages/editor.pages";
+import HomePage from "./pages/home.page";
 
 
 const App = () => { 
@@ -29,6 +30,8 @@ const App = () => {
             
             <Route path="/editor" element={<Editor />} />
             <Route path="/" element={<Navbar />}>
+
+                <Route index element={<HomePage />} />
             
             <Route path="signin" element={<UserAuthForm type="sign-in" />} />
             <Route path="signup" element={<UserAuthForm type="sign-up" />} />
---
+++ Frontend/src/pages/home.page.jsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+
+    return (
+
+        <h1>Home page coming from component jsx</h1>
+    )
+}
+
+
+export default HomePage
