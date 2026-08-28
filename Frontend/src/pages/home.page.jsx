d18a39480919d5454acc10d4974668e306f37317
Bivaas <bivaasbaral7@gmail.com>
2026-08-28 08:45:40 +0545

    latest blog fetch

--- Frontend/src/pages/home.page.jsx
+++ Frontend/src/pages/home.page.jsx
@@ -1,6 +1,23 @@
 import InPageNavigation from "../components/inpage-navigation.component";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
+
+    const [ blogs, setBlogs ] = useState(null);
+
+    const fetchLatestBlogs = () => {
+
+        axios.geet(import.meta.env.VITE_DOMAIN + "/latest-blogs")
+        .then (( { blogs }) => {
+
+            console.log(blogs);
+        }) 
+        .catch (err => {
+
+            console.log(err);
+        })
+    }
 
     return (
 
